a74ef8da1e879f0750a4f2a04ae3cd3b890c2bad
semahello <smnrkavak@gmail.com>
2025-10-22 00:40:47 +0200

    Use dynamic basename for BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import App from "./App";
 import "./index.css";
 import { LanguageProvider } from "./contexts/LanguageContext";
 
+// Production'da basename kullan, development'ta kullanma
+const basename = import.meta.env.PROD ? "/e-Ministro-1-/" : "/";
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/e-Ministro-1-/">
+    <BrowserRouter basename={basename}>
       <LanguageProvider>
         <App />
       </LanguageProvider>
